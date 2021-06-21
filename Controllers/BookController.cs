0da17814d82143640921eee2d59441d6c13a3922
kietluan456 <86236287+kietluan456@users.noreply.github.com>
2021-06-21 21:25:26 +0700

    Add files via upload

---
+++ Controllers/BookController.cs
@@ -0,0 +1,132 @@
+﻿using Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class BookController : Controller
+    {
+        private int id;
+
+        // GET: Book
+        public String Hello(string uni)
+        {
+            return "Hi Thay" + uni;
+        }
+        public ActionResult ListBook()
+        {
+            var books = new List<string>();
+            books.Add("Book1");
+            books.Add("Book2");
+            books.Add("Book3");
+            books.Add("Book4");
+            ViewBag.BOOKS = books;
+            return View();
+        }
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Xu Xu dung khoc", "Hoi Nha Sach", "/Content/Images/b1c.jpg"));
+            books.Add(new Book(2, "Nha gia kim", "Paulo Coelho", "/Content/Images/b2c.jpg" ));
+            books.Add(new Book(3, "Muon nhanh thi phai tu tu", "KB", "/Content/Images/b3c.jpg"));
+            books.Add(new Book(4, "Neu nhu yeu", "Tran Tra My", "/Content/Images/b4c.jpg"));
+            return View(books);
+        }
+
+           
+        public ActionResult EditBook(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Xu Xu dung khoc", "Hoi Nha Sach", "/Content/Images/b1c.jpg"));
+            books.Add(new Book(2, "Nha gia kim", "Paulo Coelho", "/Content/Images/b2c.jpg"));
+            books.Add(new Book(3, "Muon nhanh thi phai tu tu", "KB", "/Content/Images/b3c.jpg"));
+            books.Add(new Book(4, "Neu nhu yeu", "Tran Tra My", "/Content/Images/b4c.jpg"));
+
+            Book book = new Book();
+            foreach(Book a in books)
+            {
+                if (a.Id == id)
+                {
+                    book = a;
+
+                    break;
+                }
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);                       
+        }
+        //===================================================================================
+
+        [HttpPost, ActionName("EditBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(int id, string Title, string Author, string Image)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Xu Xu dung khoc", "Hoi Nha Sach", "/Content/Images/b1c.jpg"));
+            books.Add(new Book(2, "Nha gia kim", "Paulo Coelho", "/Content/Images/b2c.jpg"));
+            books.Add(new Book(3, "Muon nhanh thi phai tu tu", "KB", "/Content/Images/b3c.jpg"));
+            books.Add(new Book(4, "Neu nhu yeu", "Tran Tra My", "/Content/Images/b4c.jpg"));
+
+            //Book book = new Book();
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            foreach (Book a in books)
+            {
+                if (a.Id == id)
+                {
+                    a.Title = Title;
+                    a.Author = Author;
+                    a.Image = Image;
+                    break;
+                }
+            }
+
+            return View("ListBookModel", books);
+        }
+
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+
+        //=================================================================================
+
+        [HttpPost, ActionName("CreateBook")]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Contact([Bind(Include = "Id, Title, Author, Image")]Book book)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Xu Xu dung khoc", "Hoi Nha Sach", "/Content/Images/b1c.jpg"));
+            books.Add(new Book(2, "Nha gia kim", "Paulo Coelho", "/Content/Images/b2c.jpg"));
+            books.Add(new Book(3, "Muon nhanh thi phai tu tu", "KB", "/Content/Images/b3c.jpg"));
+            books.Add(new Book(4, "Neu nhu yeu", "Tran Tra My", "/Content/Images/b4c.jpg"));
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    books.Add(book);
+                }
+
+            }
+            catch (/*RetryLimitExceededException*/Exception)
+            {
+                ModelState.AddModelError("", "Error");
+            }
+            return View("ListBookModel", books);
+        }
+
+    }
+}
+
+    
---
+++ Models/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+        private string image;
+
+        public Book()
+        {
+        }
+
+        public Book(int id, string title, string author, string image)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image = image;
+        }
+
+        [Required(ErrorMessage = "Khong bo trong tieu de!")]
+        [StringLength(250,ErrorMessage = "Tieu de khong vuot qua 250 ky tu!")]
+        [Display(Name = "Tieu de")]
+
+
+        public int Id { get { return id; } set => id = value; }
+        public string Title { get { return title; } set => title = value; }
+        public string Author { get { return author; } set => author = value; }
+        public string Image { get { return image; } set => image = value; }
+
+
+    }
+}
